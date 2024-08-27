766f6c0ecf44a6a6b280aa01610387eedd5829fb
Julien Delhaye <julien.delhaye@portainer.io>
2024-08-27 18:25:30 +0200

    add backend test for logs

--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -40,15 +40,53 @@ describe("GET /container/:containerId", () => {
 
 
 describe("GET /container/:containerId/start", () => {
+    this.timeout(10000)
     const containerId = "4872800e"
     it("return 204 if success", async () => {
+        this.timeout(10000)
+
         return request(app).get(`/container/${containerId}/start`).expect(204)
     })
 
     it("return 304 if container is already running", async () => {
+        this.timeout(10000)
+
         return request(app).get(`/container/${containerId}/start`).expect(304)
     })
     it("return 404 if container not found", async () => {
+        this.timeout(10000)
+
         return request(app).get(`/container/666/start`).expect(404)
     })
 })
+
+describe("GET /container/:containerId/stop", () => {
+    this.timeout(10000)
+    const containerId = "4872800e"
+    it("return 204 if success", async () => {
+        this.timeout(10000)
+
+        return request(app).get(`/container/${containerId}/stop`).expect(200)
+    })
+
+    it("return 304 if container is already stopped", async () => {
+        this.timeout(10000)
+
+        return request(app).get(`/container/${containerId}/stop`).expect(200)
+    })
+    it("return 404 if container not found", async () => {
+        this.timeout(10000)
+
+        return request(app).get(`/container/666/stop`).expect(200)
+    })
+})
+
+describe("GET /container/:containerId/logs", () => {
+    const containerId = "4872800e"
+    it("should return logs", async () => {
+        return request(app).get(`/container/${containerId}/logs`).then((response) => {
+            expect(response.body).not.toBeNull()
+        })
+    })
+})
+
